c8386a0a783b11d896b78ad5bc72a6cfbd0caa67
Spiros Boosalis <samboosalis@gmail.com>
2016-06-06 01:52:38 +0300

    Initial Commit

---
+++ NSTypes/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NSTypes
+//
+//  Created by Michael Boosalis on 6/6/16.
+//  Copyright © 2016 Michael Boosalis, Lord Of The Game. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
